09d3f2cdf36598addd74b9fca1840749946e7064
MrM3h <ManuelRodriguez2400000@gmail.com>
2023-11-27 14:25:54 -0500

    uploading logic project

---
+++ Main.py
@@ -0,0 +1,11 @@
+age = 15
+name = "Atta Kakra Detome"
+otherName = 'kay'
+GAS_PRICE = 3.99
+# this code does something 
+print(age)
+print(name)
+print(otherName)
+print(GAS_PRICE)
+
+print("price of gas is ",GAS_PRICE)
